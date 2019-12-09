fc8652670d8c5e625b746882d6dfe68029fe8cfe
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-09 14:18:57 -0800

    add test for first name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe User, type: :model do
                       password:   'password')
 
     expect(bob).to be_valid
+    
+    bob.delete
   end
+
+  it 'is invalid without a first name' do
+    bob = User.create(first_name: '',
+                      last_name:  'Ross',
+                      email:      'happytree47@gmail.com',
+                      password:   'password')
+
+    expect(bob).to be_invalid
+
+    bob.delete
+  end
+
 end
