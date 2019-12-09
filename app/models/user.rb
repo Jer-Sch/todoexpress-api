120fbcea935867e27f73085a618b03262a9996a9
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-09 14:43:19 -0800

    add format validation and valid email regular expression

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name,  presence: true
-  validates :email,      presence: true
+  validates :email,      presence: true,
+                         format:   {with: VALID_EMAIL_REGEX}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_secure_password
+
   validates :password,   length: {minimum: 6}
 end
