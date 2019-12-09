18b6fd4725b9c1a22205fa0597fb99d0e134ae12
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-09 14:46:40 -0800

    add uniqueness and case sensitivity validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name,  presence: true
-  validates :email,      presence: true,
-                         format:   {with: VALID_EMAIL_REGEX}
-
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,      presence:       true,
+                         uniqueness:     true,
+                         case_sensitive: false,
+                         format:     {with: VALID_EMAIL_REGEX}
+
 
   has_secure_password
 
