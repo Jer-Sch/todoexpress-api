4a9a27bd8b268ff783a8c8fbb492416b9febf0e8
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-09 14:05:07 -0800

    add first user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with first name, last name, email, and password' do
+    bob = User.create(first_name: 'Bob',
+                      last_name:  'Ross',
+                      email:      'happytree47@gmail.com',
+                      password:   'password')
+
+    expect(bob).to be_valid
+  end
 end
