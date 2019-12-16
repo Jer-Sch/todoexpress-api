d007b8320da34180d5679c258ff94019ff9c44c1
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-16 13:49:21 -0800

    finish and debug todo item specs

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe TodoItem, type: :model do
-  it 'has content' do
-    first_todo = TodoItem.new(content: "Walk the dog")
+  graham = User.create!(first_name: 'Graham', 
+                        last_name:  'Parsons',
+                        email:      'gparsons@gmail.com',
+                        password:   'password')
 
-    expect(first_todo).to be_valid
+  todo = graham.todo_items.create!(content: 'Walk the dog')
+
+  it 'has content' do
+   expect(todo).to be_valid
   end
+
+  it 'belongs to a user' do
+    expect(todo.user_id).to eq(graham.id)
+  end
+
+  graham.todo_items.destroy
+  graham.destroy
 end
