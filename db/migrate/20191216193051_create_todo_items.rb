6b5525fab7c60c9b30d14e65a322936443fa01da
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-16 11:38:41 -0800

    add content and user id to todoitem migration

--- db/migrate/20191216193051_create_todo_items.rb
+++ db/migrate/20191216193051_create_todo_items.rb
@@ -1,6 +1,8 @@
 class CreateTodoItems < ActiveRecord::Migration[6.0]
   def change
     create_table :todo_items do |t|
+      t.string :content
+      t.integer :user_id
 
       t.timestamps
     end
