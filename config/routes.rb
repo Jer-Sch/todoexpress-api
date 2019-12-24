fe4515191e72a8b740eb7bc9d9ddbd2eb8f08546
Jeremy Schuurmans <schuurm@ns.codes>
2019-12-24 09:17:27 -0800

    namespace and version api in controllers and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'api' do
+    namespace :v1 do
+      resources :users
+      resources :todo_items
+    end
+  end
 end
